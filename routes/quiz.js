7f68075eda0ee52298c9c8acc57b4d1ce66f9f98
rahaf-alawwad <rhfalawwad@outlook.com>
2021-04-20 10:41:10 +0300

    added todays matches

--- routes/quiz.js
+++ routes/quiz.js
@@ -6,10 +6,21 @@ let quizzes = require("../public/quiz.json")
 
 
 
-router.get("/quiz/index", (req, res) => {
-
+router.get("/quiz/index1", (req, res) => {
+    
     res.render("quiz/index" , {quizzes});
 
 })
 
+
+
+router.get("/quiz/index", (req, res) => {
+   random = Math.floor(Math.random() * 10)
+   question=  quizzes[random].question
+   choices = [quizzes[random].choose1,quizzes[random].choose2,quizzes[random].correct]
+    shuffled = choices.sort(() => Math.random() - 0.5)
+   correct =quizzes[random].correct;
+    res.render("quiz/index2" , {question,shuffled,correct});
+
+})
 module.exports = router;
